197a0d5f0c009f5825b5f8374500a75d315eef6e
Rei-nk <wahyureihan20@gmail.com>
2026-05-19 22:47:34 +0700

    Fix: Use public MySQL host and port for database connection

--- config.php
+++ config.php
@@ -1,10 +1,11 @@
 <?php
-$host = 'localhost';
-$user = 'root';
-$pass = '';
-$dbname = 'user_login';
+$host   = 'mysql://root:DDVAmTdpsKFwnZfsRwuBdBaAyhqxEbhB@ballast.proxy.rlwy.net:50118/railway'; 
+$user   = 'root';
+$pass   = 'DDVAmTdpsKFwnZfsRwuBdBaAyhqxEbhB';
+$dbname = 'railway';
+$port   = '3306'; 
 
-$conn = mysqli_connect($host, $user, $pass, $dbname);
+$conn = mysqli_connect($host, $user, $pass, $dbname, $port);
 
 if (!$conn) {
     die("Koneksi gagal: " . mysqli_connect_error());
